4e2ff66e241246691731db2288fd1cc27ab77591
Mauricio MRV. Reyes Vergara <mreyes@lumen.local.com>
2020-06-19 21:26:53 -0500

    actualización Ron

---
+++ Ro.py
@@ -0,0 +1,13 @@
+# cadena="prueba"
+# #print(cadena[::-1])
+# #cadena[5]="o"
+# print(len(cadena))
+
+# #for x in cadena:
+# #    print(x)
+
+listaRon= ["pan", "salchicha", "mayonesa", "katsup", 2]
+print(len(listaRon))
+print(range(5))
+for e in range(5):
+    print (e)
--- Ronmel.py
+++ Ronmel.py
@@ -1,14 +1,21 @@
-lista1= ["jamón","huevito", 5, "catsup", .5, "#café", "agua"]
-lista1[1]="huevo"
+lista1= ["jamón","huevito", "5", "catsup", "#café", "agua"]
+numeros= [0,1,2,3,4,5]
+#lista1[1]="huevo"
 #print(lista1)
 #if "CATSUP".lower() in lista1:
 #    print("elemento dentro de la lista")
 #else:
 #    print("elemento no encontrado")
+print(len(lista1))
+print(range(6))
+for elemento in range(6):
+    print(elemento)
+# for elemento in range(len(lista1)):
+#     print(lista1[elemento],elemento)
+
+# a= enumerate(lista1)
+# print(list(a))  
 
 
-for elemento in lista1:
-    print(str(type(elemento)))
-
-    if str(type(elemento)) == "<class 'str'>":
-        print(elemento)
+    # if str(type(elemento)) == "<class 'str'>":
+    #     print(elemento)
